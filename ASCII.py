fe8d2dae0f9241d4da275ae753991ca14b550dce
Subhradip <subhradip6969@gmail.com>
2025-03-18 18:44:38 +0530

    printing something but not working

--- ASCII.py
+++ ASCII.py
@@ -2,7 +2,7 @@ from PIL import Image
 # String ordered according to how dense the character is
 ASCII_characters = ' .:co?9W#'
 
-def resize(image, new_width=149):
+def resize(image, new_width=148):
     width, height = image.size
     aspect_ratio = height / width
     new_height = int(new_width * aspect_ratio * 0.42)
@@ -26,23 +26,25 @@ def assign_border(square):
     elif color_diff(square[0], square[1])>30 and color_diff(square[0], square[2])<30 and color_diff(square[3], square[1])<30:
         return '|'
     else:
-        return None
+        return ' '
     
 def square_to_border(image):
     pixels = image.getdata()
-    border_list = [].append(assign_border([pixels[i*2], pixels[1+i*2], pixels[image.getdata()[0]/2+i*2], pixels[1+image.getdata()[0]/2+i*2]]) for i in range(image.size[0]*image.size[1]//4))
-    
+    border_list = []
+    print(list(pixels))
+    for i in range(image.size[0]*image.size[1]//4):
+        border_list.append(assign_border([pixels[i*2], pixels[1+i*2], pixels[image.size[0]//2+i*2], pixels[1+image.size[0]//2+i*2]]))
+    #print(border_list)
     return border_list
     
 def pixel_to_ascii(image):
     pixels = image.getdata()
-    print(list(pixels))
     # getdata() returns a list with values for each starting from the top left pixel and going right and then down
     ascii_str = ''.join(ASCII_characters[pixel // 30] for pixel in pixels) 
     # Maps to ASCII depending upon how high the value is
     return ascii_str
 
-def image_to_ascii(image_path, output_width=149):
+def image_to_ascii(image_path, output_width=148):
     try:
         image = Image.open(image_path)
     # return None ensures the lines after the except block are not executed in case of an error
@@ -52,6 +54,7 @@ def image_to_ascii(image_path, output_width=149):
     
     image = resize(image, output_width*2)
     border_list = square_to_border(image)
+    #print(border_list)
     image = resize(image, output_width)
     image = greyscale(image)
     ascii_str = pixel_to_ascii(image)
@@ -60,11 +63,14 @@ def image_to_ascii(image_path, output_width=149):
     
     ascii_str = ''
     for i in range(len(ascii_list)):
-        if border_list[i] == None:
-            ascii_str = ascii_str.join(ascii_list[i])
-        else:
-            ascii_str = ascii_str.join(border_list[i])
-    
+        try:
+            if border_list[i] == ' ':
+                ascii_str+=ascii_list[i]
+            else:
+                ascii_str+=border_list[i]
+        except IndexError:    
+            ascii_str+=ascii_list[i]
+            
     # Formatting the ASCII string
     ascii_str = '\n'.join(ascii_str[i:i+output_width] for i in range(0, len(ascii_str), output_width))
     
@@ -73,5 +79,5 @@ def image_to_ascii(image_path, output_width=149):
     #file_name = input('Making sure to include the .txt extension, \n Please enter a file name for the .txt file: ')
     #    f.write(ascii_str)
 
-image_path = input('Please enter the image path: ')
+image_path = 'half.png'#input('Please enter the image path: ')
 image_to_ascii(image_path)
