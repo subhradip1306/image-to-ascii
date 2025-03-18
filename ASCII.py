7b60b5434a60ae397efb68339134f75ddb52edc4
Subhradip <subhradip6969@gmail.com>
2025-03-18 22:10:46 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -2,14 +2,14 @@ from PIL import Image
 # String ordered according to how dense the character is
 ASCII_characters = ' .:co?9W#'
 
-def resize(image, new_width=203):
+def resize(image, new_width=180):
     width, height = image.size
     aspect_ratio = height / width
     new_height = int(new_width * aspect_ratio * 0.47)
 
     return image.resize((new_width, new_height))
 
-def greyscale(image):
+def greyscale(image): # getdata() for a greyscale image will return the brightness value instead of (r,g,b,a)  
     return image.convert('L')
 
 def diff(pixel1, pixel2):
@@ -23,21 +23,25 @@ def pixel_to_ascii(image, grey_image):
     ascii_str = ''
     for i in range(len(list(pixels))):
             try:
-                if diff(pixels[i], pixels[i+1])>50:
+                if diff(pixels[i], pixels[i+1])>90:
                     ascii_str += '|' 
 		            # Enters a vertical border when two consecutive pixels have a very different color
-                elif diff(pixels[i], pixels[i+149])>50:
+                elif diff(pixels[i], pixels[i+180])>90:
                     ascii_str += '-'
+			    # Similarly, a horizontal border
                 else:
 	                ascii_str += ASCII_characters[grey_pixels[i] // 30] 
-                    # Maps to ASCII depending upon how high the value is
+                    # Maps to ASCII depending upon how high the brightness value is
 
-            except IndexError:
+            except TypeError: # Caused when a pixel is empty (pixels[i] becomes 0)
+                ascii_str += ASCII_characters[pixels[i] // 30]
+				
+            except IndexError: # Caused when pixels[i] is the last pixel
                 continue
 
     return ascii_str
 
-def image_to_ascii(image_path, output_width=203):
+def image_to_ascii(image_path, output_width=180):
     try:
         image = Image.open(image_path)
     # return None ensures the lines after the except block are not executed in case of an error
@@ -54,9 +58,9 @@ def image_to_ascii(image_path, output_width=203):
     
     print(ascii_str)
     # Writes the .txt file
-    #file_name = input('Making sure to include the .txt extension, \n Please enter a file name for the .txt file: ')
-    #with open(file_name, 'w') as f:
-    #    f.write(ascii_str)
+    file_name = input('Making sure to include the .txt extension, \n Please enter a file name for the .txt file: ')
+    with open(file_name, 'w') as f:
+        f.write(ascii_str)
 
-image_path ='zelda.jpg' #input('Please enter the image path: ')
+image_path =input('Please enter the image path: ')
 image_to_ascii(image_path)
